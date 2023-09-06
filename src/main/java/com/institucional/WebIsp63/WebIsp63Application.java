4cb8a6a16046b05446811fee7b46e7a72c965a1c
Analia Casali <acasali.ac@gmail.com>
2023-09-05 21:29:53 -0300

    First Commit - New Project

---
+++ src/main/java/com/institucional/WebIsp63/WebIsp63Application.java
@@ -0,0 +1,13 @@
+package com.institucional.WebIsp63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIsp63Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIsp63Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/institucional/WebIsp63/WebIsp63ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institucional.WebIsp63;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIsp63ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
